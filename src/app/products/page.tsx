9a4ba38f2e005e3958d69537193f5cd21d6a8639
enkhbold470 <enkhbold470@gmail.com>
2024-12-08 20:29:45 -0800

    Fetch product data from server using environment variable and update ProductsPage to display loading state

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,9 +4,9 @@ import { useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import products from "@/data/laptops.json"
-import fake from "@/data/laptops.json"
 import { Star } from "lucide-react"
 
+import { env } from "@/env.mjs"
 import { Product, ProductResponse } from "@/types/productTypes"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
@@ -15,23 +15,30 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card"
 export default function ProductsPage() {
   const [products, setProducts] = useState<Product[]>([])
 
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     const response = await fetch(
-  //       "http://192.168.0.28/api/laptops?popoulate=images"
-  //     )
-  //     const data: ProductResponse = await response.json()
-  //     return data
-  //   }
-  //   fetchProducts().then((data: ProductResponse) => setProducts(data.data))
-  // }, [])
-  // setProducts(fake.data)
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        `http://${env.NEXT_PUBLIC_SERVER_URL}/api/laptops?populate=images`
+      )
 
+      const data = await response.json()
+      console.log(data)
+      return data
+    }
+    fetchProducts().then((data) => setProducts(data.data))
+  }, [])
+  //loading data
+  if (products.length === 0)
+    return (
+      <div className="flex animate-pulse items-center justify-center text-2xl">
+        Loading...
+      </div>
+    )
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="mb-8 text-3xl font-bold">Манай Дэлгүүрт</h1>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {fake.data.map((product) => (
+        {products.map((product) => (
           <Card key={product.id} className="flex flex-col">
             <CardContent className="p-4">
               <div className="relative mb-4 aspect-square">
@@ -62,18 +69,27 @@ export default function ProductsPage() {
               </div> */}
               <div className="mb-2 flex items-baseline">
                 <span className="text-2xl font-bold">
-                  ${product.price.toFixed(2)}
+                  ₮
+                  {product.price
+                    .toFixed()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </span>
                 <span className="ml-2 text-sm text-gray-500 line-through">
-                  ${product.originalPrice.toFixed(2)}
+                  ₮
+                  {product.originalPrice
+                    .toFixed()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </span>
-                <Badge className="ml-2">
-                  Хэмнэлт ${(product.originalPrice - product.price).toFixed(2)}
+                <Badge variant="destructive" className="ml-2">
+                  Хэмнэлт ₮
+                  {(product.originalPrice - product.price)
+                    .toFixed()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </Badge>
               </div>
             </CardContent>
             <CardFooter className="mt-auto">
-              <Button asChild className="w-full">
+              <Button variant="default" asChild className="w-full">
                 <Link href={`/products/${product.id}`}>Дэлгэрэнгүй</Link>
               </Button>
             </CardFooter>
